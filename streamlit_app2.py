f7dedec384dfb33304e94605eda10eb30443d93a
navdeep9827 <108651474+navdeep9827@users.noreply.github.com>
2024-05-20 18:07:00 +0530

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -3,11 +3,14 @@ import requests
 import cv2
 import numpy as np
 import base64
+from PIL import Image
 import subprocess
+import os
 import time
 
 # Function for starting Flask server
 def start_flask():
+    # Command for starting the Flask server
     command = ["python", "app.py"]
     flask_process = subprocess.Popen(command)
     return flask_process
@@ -16,49 +19,39 @@ def start_flask():
 flask_process = start_flask()
 
 # Waiting for the Flask server to start
-time.sleep(15)
+time.sleep(3)
 
 st.title("Indian Sign Language Recognition")
 
-# Setting up webcam capture
-stframe = st.empty()
-video_capture = cv2.VideoCapture(0)
+# Upload image file
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 
-# Check if the webcam is opened successfully
-if not video_capture.isOpened():
-    st.error("Error: Could not open video stream.")
-    flask_process.terminate()
-    st.stop()
+def send_image_to_flask(image):
+    # Convert image to JPEG format
+    img = Image.open(image)
+    img = img.convert('RGB')
+    img.save("temp.jpg", format="JPEG")
 
-def send_frame_to_flask(frame):
-    _, buffer = cv2.imencode('.jpg', frame)
-    frame_bytes = base64.b64encode(buffer).decode('utf-8')
+    # Read JPEG image and encode it to base64
+    with open("temp.jpg", "rb") as file:
+        image_bytes = base64.b64encode(file.read()).decode("utf-8")
+
+    # Send image to Flask for prediction
     try:
-        response = requests.post('https://mlops-project-r08k.onrender.com/predict', json={'data': frame_bytes})
+        response = requests.post('http://localhost:5000/predict', json={'data': image_bytes})
         response.raise_for_status()
-        return response.json().get('prediction', None)
+        return response.json()['prediction']
     except requests.exceptions.RequestException as e:
         st.error(f"Error connecting to Flask server: {e}")
         return None
 
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        st.error("Failed to capture video.")
-        break
-    
-    # Displaying the captured frame
-    stframe.image(frame, channels="BGR")
-
-    # Sending frame to Flask for prediction
-    prediction = send_frame_to_flask(frame)
+# Display prediction
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    prediction = send_image_to_flask(uploaded_file)
     if prediction is not None:
         st.write(f"Prediction: {prediction}")
-    else:
-        st.write("No prediction received.")
 
-# Releasing the video capture object
-video_capture.release()
-
-# Terminating the Flask process when Streamlit stops
+# Terminating the flask process when streamlit is stopped
 flask_process.terminate()
